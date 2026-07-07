d9648140815cf4dba0744dc3cef2c6b68958b471
bihanudeera-creator <bihanudeera@gmail.com>
2026-07-07 07:31:19 +0530

    update parrollform.designer.cs

---
+++ C#projectviva/C#projectviva/8_Member_Project/08_Payroll/Forms/PayrollForm.Designer.cs
@@ -0,0 +1,49 @@
+namespace TeaEstate
+{
+    partial class PayrollForm
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        
+        private void InitializeComponent()
+        {
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.grpDetails = new System.Windows.Forms.GroupBox();
+            this.lblWorker = new System.Windows.Forms.Label();
+            this.cmbWorker = new System.Windows.Forms.ComboBox();
+            this.lblMonth = new System.Windows.Forms.Label();
+            this.txtMonth = new System.Windows.Forms.TextBox();
+            this.lblDaysWorked = new System.Windows.Forms.Label();
+            this.txtDaysWorked = new System.Windows.Forms.TextBox();
+            this.lblDailyRate = new System.Windows.Forms.Label();
+            this.txtDailyRate = new System.Windows.Forms.TextBox();
+            this.lblYieldBonus = new System.Windows.Forms.Label();
+            this.txtYieldBonus = new System.Windows.Forms.TextBox();
+            this.lblPaidDate = new System.Windows.Forms.Label();
+            this.dtpPaidDate = new System.Windows.Forms.DateTimePicker();
+            this.lblTotalCaption = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.dgvSalary = new System.Windows.Forms.DataGridView();
+            this.pnlHeader.SuspendLayout();
+            this.grpDetails.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSalary)).BeginInit();
+            this.SuspendLayout();
